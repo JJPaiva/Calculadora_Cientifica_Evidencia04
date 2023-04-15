1074ff70da700e5457eaa131d8548f43f24def32
L1TnzO <thenzo22@gmail.com>
2023-04-15 16:53:03 -0400

    Agrega metodo para calcular ecuaciones de la recta

---
+++ src/main/java/org/example/EcuacionRecta.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class EcuacionRecta {
+
+    public static double[] calcularEcuacionRecta(double[] puntoInicial, double[] puntoFinal) {
+        if (puntoInicial[0] == puntoFinal[0]) {
+            throw new ArithmeticException("Los puntos tienen la misma coordenada X, no se puede calcular la recta");
+        }
+
+        double m = (puntoFinal[1] - puntoInicial[1]) / (puntoFinal[0] - puntoInicial[0]);
+        double b = puntoInicial[1] - m * puntoInicial[0];
+
+        return new double[] { m, b };
+    }
+}
+
