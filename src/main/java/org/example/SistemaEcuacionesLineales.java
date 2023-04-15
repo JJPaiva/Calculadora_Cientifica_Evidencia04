ec99b27ce2d7b9cc9b3f4007f77abf18e68095b9
L1TnzO <thenzo22@gmail.com>
2023-04-15 17:12:07 -0400

    Agrega metodo para calcular sistemas de ecuaciones lineales 2x2

--- src/main/java/org/example/SistemaEcuacionesLineales.java
+++ src/main/java/org/example/SistemaEcuacionesLineales.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class SistemaEcuacionesLineales {
 
-    public static double[] resolverSistema2x2(double[][] coeficientes, double[] constantes) {
+    public static double[] resolverSistemaEcuacionesLineales(double[][] coeficientes, double[] constantes) {
         double det = coeficientes[0][0] * coeficientes[1][1] - coeficientes[0][1] * coeficientes[1][0];
         if (det == 0) {
             throw new ArithmeticException("El sistema no tiene solución única");
