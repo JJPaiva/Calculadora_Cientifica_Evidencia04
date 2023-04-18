1901849c1a3055ffa0f7d4c802599149228d1050
L1TnzO <59234471+L1TnzO@users.noreply.github.com>
2023-04-18 15:46:04 -0400

    Update EcuacionRectaTest.java

--- src/test/java/org/example/EcuacionRectaTest.java
+++ src/test/java/org/example/EcuacionRectaTest.java
@@ -1,10 +1,25 @@
 package org.example;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class EcuacionRectaTest {
+class EcuacionRectaTest {
+
     @Test
-    public void testResolverEcuacionRecta() {
+    void testResolverEcuacionRecta_calculoCorrecto() {
+        double[] puntoInicial = {1, 2};
+        double[] puntoFinal = {3, 6};
+        double[] resultadoEsperado = {2, 0};
+        double[] resultadoObtenido = EcuacionRecta.resolverEcuacionRecta(puntoInicial, puntoFinal);
 
+        assertArrayEquals(resultadoEsperado, resultadoObtenido, 1e-6);
+    }
+
+    @Test
+    void testResolverEcuacionRecta_mismaCoordenadaX() {
+        double[] puntoInicial = {1, 2};
+        double[] puntoFinal = {1, 6};
+
+        assertThrows(ArithmeticException.class, () -> EcuacionRecta.resolverEcuacionRecta(puntoInicial, puntoFinal));
     }
 }
