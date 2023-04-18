1cebf0662742241fbc81342928a5558de0677710
L1TnzO <59234471+L1TnzO@users.noreply.github.com>
2023-04-18 15:42:21 -0400

    Update EcuacionCuadraticaTest.java

--- src/test/java/org/example/EcuacionCuadraticaTest.java
+++ src/test/java/org/example/EcuacionCuadraticaTest.java
@@ -1,10 +1,38 @@
 package org.example;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class EcuacionCuadraticaTest {
+class EcuacionCuadraticaTest {
+
     @Test
-    public void testResolverEcuacionCuadratica() {
+    void testResolverEcuacionCuadratica_solucionesReales() {
+        double[] coeficientes = {1, -3, 2};
+        double[] solucionesEsperadas = {2, 1};
+        double[] solucionesObtenidas = EcuacionCuadratica.resolverEcuacionCuadratica(coeficientes);
+
+        assertArrayEquals(solucionesEsperadas, solucionesObtenidas, 1e-6);
+    }
+
+    @Test
+    void testResolverEcuacionCuadratica_noSolucionesReales() {
+        double[] coeficientes = {1, 2, 3};
         
+        assertThrows(IllegalArgumentException.class, () -> EcuacionCuadratica.resolverEcuacionCuadratica(coeficientes));
+    }
+
+    @Test
+    void testResolverEcuacionCuadratica_coeficienteACero() {
+        double[] coeficientes = {0, 2, 3};
+        
+        assertThrows(IllegalArgumentException.class, () -> EcuacionCuadratica.resolverEcuacionCuadratica(coeficientes));
+    }
+
+    @Test
+    void testResolverEcuacionCuadratica_argumentoInvalido() {
+        double[] coeficientes = {1, 2};
+        
+        assertThrows(IllegalArgumentException.class, () -> EcuacionCuadratica.resolverEcuacionCuadratica(coeficientes));
     }
 }
+
