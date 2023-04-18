57aaf7266b452e3090f6d8a92f50d3535d41f43c
L1TnzO <59234471+L1TnzO@users.noreply.github.com>
2023-04-18 17:00:03 -0400

    Update SistemaEcuacionesLinealesTest.java

--- src/test/java/org/example/SistemaEcuacionesLinealesTest.java
+++ src/test/java/org/example/SistemaEcuacionesLinealesTest.java
@@ -1,10 +1,26 @@
 package org.example;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class SistemaEcuacionesLinealesTest {
+class SistemaEcuacionesLinealesTest {
+
     @Test
-    public void testResolverSistemaEcuacionesLineales() {
+    void testResolverSistemaEcuacionesLineales_solucionUnica() {
+        double[][] coeficientes = {{2, -1}, {3, 4}};
+        double[] constantes = {1, 7};
+        double[] resultadosEsperados = {1, 1};
+        double[] resultadosObtenidos = SistemaEcuacionesLineales.resolverSistemaEcuacionesLineales(coeficientes, constantes);
 
+        assertArrayEquals(resultadosEsperados, resultadosObtenidos, 1e-6);
+    }
+
+    @Test
+    void testResolverSistemaEcuacionesLineales_sinSolucionUnica() {
+        double[][] coeficientes = {{1, -1}, {2, -2}};
+        double[] constantes = {2, 4};
+
+        assertThrows(ArithmeticException.class, () -> SistemaEcuacionesLineales.resolverSistemaEcuacionesLineales(coeficientes, constantes));
     }
 }
+
